8f0c614b936c3a152fee60b8a2b6d61802394be5
Soramichi Akiyama <akiyama@m.soramichi.jp>
2017-03-25 13:04:30 +0900

    Show the thumbnail of the uploaded image

--- src/ts/rtm.ts
+++ src/ts/rtm.ts
@@ -331,6 +331,12 @@ for(var i in rtms){
     let text: string = extract_text(message, user_list, emoji_list);
     let table = $("#main_table");
 
+    let shared_file_image_id;
+    if(!!message["file"] && message["file"]["mimetype"].indexOf("image") != -1) { // file_shared
+      shared_file_image_id = Math.random().toString(36).slice(-8);
+      text += "<img id='" + shared_file_image_id + "' src='' />";
+    }
+
     let ts_date: Date = new Date(new Date(Number(ts)*1000));
     let ts_hour: string = ts_date.getHours().toString();
     ts_hour = Number(ts_hour) < 10 ? "0" + ts_hour : ts_hour;
@@ -364,6 +370,10 @@ for(var i in rtms){
     let record: string = "<tr id='" + tr_id +
       "' style='" + style + "'>"+ image_column + text_column + "</tr>";
     table.prepend(record);
+
+    if(!!shared_file_image_id) {
+	get_image(get_maximum_thumbnail(message["file"]), token, shared_file_image_id, message["file"]["mimetype"]);
+    }
 
     var button = $("#" + button_id);
     $("#" + button_id).click(function() {
--- src/ts/util.ts
+++ src/ts/util.ts
@@ -48,3 +48,30 @@ function get_team_info(token: string, team_info: {}) {
         }
     });
 }
+
+// Note: $.ajax does not properly recieve binary files,
+// so we need to use XMLHttpRequest directly.
+// http://www.henryalgus.com/reading-binary-files-using-jquery-ajax/
+function get_image(image_url: string, token: string, tag_id: string, mime: string) {
+    let request = new XMLHttpRequest();
+
+    request.open("GET", image_url, /*async: */true);
+    request.responseType = "arraybuffer";
+    request.setRequestHeader("Authorization", "Bearer " + token);
+
+    request.onreadystatechange = function(){
+	let image_base64 = new Buffer(this.response).toString('base64');
+	$("#" + tag_id)[0]["src"] = "data:" + mime + ";base64," + image_base64;
+    }
+
+    request.send();
+}
+
+function get_maximum_thumbnail(file: {}): string{
+    let sizes: string[] = ["480", "360", "160", "80", "64"];
+
+    for(var i in sizes){
+	if(!!file["thumb_" + sizes[i]])
+	    return file["thumb_" + sizes[i]];
+    }
+}
